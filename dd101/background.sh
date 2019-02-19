9ddf4200dcc599bc8299988ba21de22ba59fdc47
technovangelist <mattw@datadoghq.com>
2019-02-19 14:59:49 -0500

    asth

--- dd101/background.sh
+++ dd101/background.sh
@@ -1,14 +1,14 @@
-touch test.txt
+touch /roottest.txt
 printf "This script will download the Training Environment.\033[0m  \n\n"
 
 
 printf "\033[31mDownloading the Training Environment from Github \033[0m\n"
-outputfilename=TrainingEnvironment-$(date +%m%Y%d)
+outputfilename=/root/TrainingEnvironment-$(date +%m%Y%d)
 curl -L -J https://github.com/DataDog/TrainingEnvironment/archive/master.zip -o $outputfilename
 printf "\033[31mUnzipping the Training Environment \033[0m\n"
 unzip -q $outputfilename
-mv TrainingEnvironment-master/Datadog101/* .
-rm -rf TrainingEnvironment-master
+mv /root/TrainingEnvironment-master/Datadog101/* /root
+rm -rf /root/TrainingEnvironment-master
 
 printf "\033[31mConfiguring... \033[0m\n"
-printf "#!/bin/bash\nDD_API_KEY='$apikey'\n"> .ddtraining.sh
+printf "#!/bin/bash\nDD_API_KEY='$apikey'\n"> /root/.ddtraining.sh
