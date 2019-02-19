3432a34b2c1315e3dc135f211131184904292599
technovangelist <mattw@datadoghq.com>
2019-02-19 16:23:36 -0500

    status

--- dd101/background.sh
+++ dd101/background.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-echo "this is a test"
 printf "This script will download the Training Environment.\033[0m  \n\n"
+echo "starting" > status.txt
 
 
 printf "\033[31mDownloading the Training Environment from Github \033[0m\n"
 outputfilename=/root/TrainingEnvironment-$(date +%m%Y%d)
+echo "downloading" > status.txt
 curl -L -J https://github.com/DataDog/TrainingEnvironment/archive/master.zip -o $outputfilename
+echo "unzipping" > status.txt
 printf "\033[31mUnzipping the Training Environment \033[0m\n"
 unzip -q $outputfilename
 mv /root/TrainingEnvironment-master/Datadog101/* /root
@@ -14,4 +16,5 @@ rm -rf /root/TrainingEnvironment-master
 printf "\033[31mConfiguring... \033[0m\n"
 printf "#!/bin/bash\nDD_API_KEY='$apikey'\n"> /root/.ddtraining.sh
 chmod +x /root/webprovision.sh
+echo "running web provision" > status.txt
 /root/webprovision.sh
