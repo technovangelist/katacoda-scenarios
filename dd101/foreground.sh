c0dec1e9b97cfc022e416c26bd31045402e2fc65
technovangelist <mattw@datadoghq.com>
2019-02-19 16:42:37 -0500

    asht

--- dd101/foreground.sh
+++ dd101/foreground.sh
@@ -1,4 +1,8 @@
-#!/bin/sh -x
+#!/bin/bash -ex
+{ set +x; } 2>/dev/null
+echo "shell tracing is disabled here"; set -x;
+echo "but is enabled here"
+
 alias echo='{ set +x; } 2> /dev/null; builtin echo'
 echo "Building out the Learning Environment."
   
