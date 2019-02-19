8a6988de0978cd5cc41dca84fa6aff769cfd93ed
technovangelist <mattw@datadoghq.com>
2019-02-19 14:56:22 -0500

    asht

--- dd101/background.sh
+++ dd101/background.sh
@@ -1,5 +1,5 @@
-printf "This script will download the Training Environment. It is installed into the current directory:\n\n    $installdir  \n\n
-If you want to move this to a different directory, the easiest way is to delete the directory \nand run the command from the Learning Center again.\033[0m  \n\n"
+touch test.txt
+printf "This script will download the Training Environment.\033[0m  \n\n"
 
 
 printf "\033[31mDownloading the Training Environment from Github \033[0m\n"
