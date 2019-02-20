52542f0e2a8217bcebeb383aa1954ae70ea3950c
technovangelist <mattw@datadoghq.com>
2019-02-20 11:21:42 -0500

    ashtat

--- dd101/foreground.sh
+++ dd101/foreground.sh
@@ -1,3 +1,14 @@
 #!/bin/bash 
 sleep 1
-prepenvironment
+# prepenvironment
+apt update --yes
+# sleep 1
+# echo "install spc" > status.txt
+apt install software-properties-common --yes
+# sleep 1
+# echo "add repo" > status.txt
+apt-add-repository --yes --update ppa:ansible/ansible
+# sleep 1
+# echo "install ansible" > status.txt
+apt --yes install ansible
+# echo "ansible ready" > status.txt
