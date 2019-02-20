f8ff2cb6f7f95db83813af5c4c8f75c722875e5a
technovangelist <mattw@datadoghq.com>
2019-02-20 10:50:26 -0500

    at

--- dd101/background.sh
+++ dd101/background.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -x
 
 
-echo "starting" > status.txt
+echo "pre start" > status.txt
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get --yes install ansible
+echo "ansible ready" > status.txt
 
 
 outputfilename=/root/TrainingEnvironment-$(date +%m%Y%d)
