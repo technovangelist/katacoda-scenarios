6d6e0af2ae28a3c809201876bd1e7a110e099144
technovangelist <mattw@datadoghq.com>
2019-02-20 13:40:01 -0500

    ddinstall

--- dd101/foreground.sh
+++ dd101/foreground.sh
@@ -2,16 +2,5 @@
 clear
 sleep 1
 prepenvironment
-# apt update --yes
-# sleep 1
-# echo "install spc" > status.txt
-# apt install software-properties-common --yes
-# sleep 1
-# echo "add repo" > status.txt
-# apt-add-repository --yes --update ppa:ansible/ansible
-# sleep 1
-# echo "install ansible" > status.txt
-# apt --yes install ansible
-# echo "ansible ready" > status.txt
-ansible all -m ping
+ansible-galaxy install Datadog.datadog
 ansible-playbook /root/ansible/dd101.yaml
