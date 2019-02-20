d3f37336c6bd598e00677942b2fe0d6fd62f96bf
technovangelist <mattw@datadoghq.com>
2019-02-20 10:29:31 -0500

    astast

--- dd101/background.sh
+++ dd101/background.sh
@@ -4,7 +4,6 @@
 echo "starting" > status.txt
 
 
-printf "\033[31mDownloading the Training Environment from Github \033[0m\n"
 outputfilename=/root/TrainingEnvironment-$(date +%m%Y%d)
 echo "downloading" > status.txt
 curl -L -J https://github.com/DataDog/TrainingEnvironment/archive/master.zip -o $outputfilename
@@ -15,8 +14,12 @@ mv /root/TrainingEnvironment-master/Datadog101/* /root
 rm -rf /root/TrainingEnvironment-master
 
 printf "\033[31mConfiguring... \033[0m\n"
-printf "#!/bin/bash\nDD_API_KEY='$apikey'\n"> /root/.ddtraining.sh
-chmod +x /root/webprovision.sh
-echo "running web provision" > status.txt
-# /root/webprovision.sh
+printf "#!/bin/bash\nDD_API_KEY='$DD_API_KEY'\n"> /root/.ddtraining.sh
+
+echo "Installing the Agent" > status.txt
+DD_API_KEY=${DD_API_KEY} bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
+
+
+
+
 echo "done" > status.txt
