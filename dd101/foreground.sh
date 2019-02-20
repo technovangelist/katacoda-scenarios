7d6b44b38e4d72e7707661164fa70e36d54bea45
technovangelist <mattw@datadoghq.com>
2019-02-20 18:05:11 -0500

    shtg

--- dd101/foreground.sh
+++ dd101/foreground.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
 clear
-# ps ef | grep -v grep | grep snapd
-# sleep 1
-# prepenvironment
+ps cax | grep -v grep | grep snapd
+sleep 1
+prepenvironment
+ps cax | grep -v grep | grep snapd
 
 # sleep 10
 # ansible-playbook /root/ansible/dd101.yaml
