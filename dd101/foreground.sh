d94b1e1bf47390329d02cbce0e813ad52b986100
technovangelist <mattw@datadoghq.com>
2019-02-20 15:04:03 -0500

    asht

--- dd101/foreground.sh
+++ dd101/foreground.sh
@@ -2,7 +2,6 @@
 clear
 sleep 1
 prepenvironment
-ps -ax|grep apt
-# ansible-galaxy install Datadog.datadog
-# sleep 1
-# ansible-playbook /root/ansible/dd101.yaml
+ansible-galaxy install Datadog.datadog
+sleep 1
+ansible-playbook /root/ansible/dd101.yaml
