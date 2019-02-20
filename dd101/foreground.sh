d3be01bca5a953987512886907d5fb071ab814dd
technovangelist <mattw@datadoghq.com>
2019-02-20 14:50:18 -0500

    is apt running

--- dd101/foreground.sh
+++ dd101/foreground.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 clear
-sleep 1
-prepenvironment
-ansible-galaxy install Datadog.datadog
-sleep 1
-ansible-playbook /root/ansible/dd101.yaml
+ps -ax|grep apt
+# sleep 1
+# prepenvironment
+# ansible-galaxy install Datadog.datadog
+# sleep 1
+# ansible-playbook /root/ansible/dd101.yaml
