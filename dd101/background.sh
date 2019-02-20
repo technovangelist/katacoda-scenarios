34203811a321ff0e02cd57e417fe495cdaa21ca7
technovangelist <mattw@datadoghq.com>
2019-02-20 11:20:22 -0500

    ashtast

--- dd101/background.sh
+++ dd101/background.sh
@@ -2,17 +2,17 @@
 
 
 echo "update" > status.txt
-apt update --yes
-sleep 1
-echo "install spc" > status.txt
-apt install software-properties-common --yes
-sleep 1
-echo "add repo" > status.txt
-apt-add-repository --yes --update ppa:ansible/ansible
-sleep 1
-echo "install ansible" > status.txt
-apt --yes install ansible
-echo "ansible ready" > status.txt
+# apt update --yes
+# sleep 1
+# echo "install spc" > status.txt
+# apt install software-properties-common --yes
+# sleep 1
+# echo "add repo" > status.txt
+# apt-add-repository --yes --update ppa:ansible/ansible
+# sleep 1
+# echo "install ansible" > status.txt
+# apt --yes install ansible
+# echo "ansible ready" > status.txt
 
 
 outputfilename=/root/TrainingEnvironment-$(date +%m%Y%d)
