134fa3037c58208f98382a3724fa470a48e9e6ea
technovangelist <mattw@datadoghq.com>
2019-02-21 08:01:22 -0500

    sshtgsgth

--- dd101/background.sh
+++ dd101/background.sh
@@ -5,4 +5,6 @@ apt-add-repository --yes --update ppa:ansible/ansible
 apt --yes install ansible
 
 ansible-galaxy install Datadog.datadog
+echo "Installing Services"> status.txt
+ansible-playbook /root/ansible/dd101.yaml
 echo "done" > status.txt
--- dd101/foreground.sh
+++ dd101/foreground.sh
@@ -2,4 +2,4 @@
 clear
 sleep 1
 prepenvironment
-ansible-playbook /root/ansible/dd101.yaml
+
