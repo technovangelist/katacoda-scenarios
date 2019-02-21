a9d530dd7d91a9b85ef96930a06ef0571ac03e92
technovangelist <mattw@datadoghq.com>
2019-02-21 07:25:15 -0500

    ashtat

--- dd101-2/foreground.sh
+++ dd101-2/foreground.sh
@@ -2,9 +2,9 @@
 clear
 sleep 1
 prepenvironment
-ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null host03 "/root/provision/webprovision.sh&"
+ssh -oStrictHostKeyChecking=no host03 "/root/provision/webprovision.sh&"
 
-ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null host04 "/root/provision/webprovision.sh&"
+ssh -oStrictHostKeyChecking=no host04 "/root/provision/webprovision.sh&"
 
-ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null host05 "/root/provision/webprovision.sh&"
+ssh -oStrictHostKeyChecking=no host05 "/root/provision/webprovision.sh&"
 
