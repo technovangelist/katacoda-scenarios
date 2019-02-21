d8677c1ec7b96510d8e096dbb1ad8492554a2ede
technovangelist <mattw@datadoghq.com>
2019-02-21 07:37:10 -0500

    shtgshgt

--- dd101-2/foreground.sh
+++ dd101-2/foreground.sh
@@ -2,13 +2,13 @@
 clear
 sleep 1
 prepenvironment
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /root/provision/webprovision.sh host03:/root/provision/webprovision.sh
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /root/provision/webprovision.sh host04:/root/provision/webprovision.sh
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /root/provision/webprovision.sh host05:/root/provision/webprovision.sh
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null webprovision.sh host03:webprovision.sh
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null webprovision.sh host04:webprovision.sh
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null webprovision.sh host05:webprovision.sh
 
-ssh -oStrictHostKeyChecking=no host03 "/root/provision/webprovision.sh&"
+ssh -oStrictHostKeyChecking=no host03 "webprovision.sh&"
 
-ssh -oStrictHostKeyChecking=no host04 "/root/provision/webprovision.sh&"
+ssh -oStrictHostKeyChecking=no host04 "webprovision.sh&"
 
-ssh -oStrictHostKeyChecking=no host05 "/root/provision/webprovision.sh&"
+ssh -oStrictHostKeyChecking=no host05 "webprovision.sh&"
 
