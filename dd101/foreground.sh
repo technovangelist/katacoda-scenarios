2db8d38d2eef624767b85e55b4e12d1b2faa561b
technovangelist <mattw@datadoghq.com>
2019-02-22 14:25:53 -0500

    asht

--- dd101/foreground.sh
+++ dd101/foreground.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 if [ ! -f "/root/provisioned" ]; then
-  cat > ddapikey.yml <<EOL
-  ---
-  datadog_api_key: ${DD_API_KEY}
-  EOL
-  clear
-  sleep 1
+cat > ddapikey.yml <<EOL
+---
+datadog_api_key: ${DD_API_KEY}
+EOL
+clear
+sleep 1
 
-  prepenvironment
+prepenvironment
 fi
 # ansible-playbook /root/ansible/dd101.yaml
