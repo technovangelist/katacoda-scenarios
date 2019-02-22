b348889b5c1d77c75d69d73ebe791c9d0a978f41
technovangelist <mattw@datadoghq.com>
2019-02-22 14:32:04 -0500

    asht

--- dd101/background.sh
+++ dd101/background.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ ! -f "/root/provisioned" ]; then
-  echo "Setup Installation Framework" > status.txt
-  apt-add-repository --yes --update ppa:ansible/ansible
-  apt --yes install ansible
+echo "Setup Installation Framework" > status.txt
+apt-add-repository --yes --update ppa:ansible/ansible
+apt --yes install ansible
 
-  ansible-galaxy install Datadog.datadog
+ansible-galaxy install Datadog.datadog
 
-  echo "Installing Services"> status.txt
-  ansible-playbook /root/ansible/dd101.yaml
-  echo "done" > status.txt
+echo "Installing Services"> status.txt
+ansible-playbook /root/ansible/dd101.yaml
+echo "done" > status.txt
 fi
