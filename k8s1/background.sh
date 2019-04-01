eb3690af5e7757cf1c34cfbc0311c98382aa80a1
technovangelist <mattw@datadoghq.com>
2019-04-01 13:49:36 -0400

    asht

--- k8s1/background.sh
+++ k8s1/background.sh
@@ -3,7 +3,7 @@ touch status.txt
 wall -n "Cloning the Github Repo"
 git clone https://github.com/burningion/distributed-tracing-with-apm-workshop trace
 cd trace
-wall "Checking out the right branch"
+wall -n "Checking out the right branch"
 git checkout k8s-autodiscovery
 cd ..
 wall -n "Getting everything into the right place"
--- k8s1/foreground.sh
+++ k8s1/foreground.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+while [ ! -f "/usr/local/bin/prepenvironment" ]; do
+  echo "."
+done
 cat > ddapikey.yml <<EOL
 ---
 datadog_api_key: ${DD_API_KEY}
