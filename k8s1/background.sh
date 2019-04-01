5f14c4d39f715cb6e7b302aa754d3abc0c6aa1fc
technovangelist <mattw@datadoghq.com>
2019-04-01 13:30:30 -0400

    asht

--- k8s1/background.sh
+++ k8s1/background.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+wall -n "Cloning the Github Repo"
 git clone https://github.com/burningion/distributed-tracing-with-apm-workshop trace
 cd trace
+wall -n "Checking out the right branch"
 git checkout k8s-autodiscovery
 cd ..
+wall -n "Getting everything into the right place"
 mv trace/* .
+wall -n "OK, you are good to get started"
