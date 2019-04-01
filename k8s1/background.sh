4f3abcb80f54aefe6f2eff8d5fa6f20096abe0c8
technovangelist <mattw@datadoghq.com>
2019-04-01 13:23:50 -0400

    asht

--- k8s1/background.sh
+++ k8s1/background.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir /tracing-workshop
-git clone https://github.com/burningion/distributed-tracing-with-apm-workshop .
+git clone --bare https://github.com/burningion/distributed-tracing-with-apm-workshop ./
 git checkout k8s-autodiscovery
