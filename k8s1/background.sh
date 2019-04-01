92e763a1ab7b2f4b68344154061391d0c0164e0d
technovangelist <mattw@datadoghq.com>
2019-04-01 14:40:23 -0400

    asht

--- k8s1/background.sh
+++ k8s1/background.sh
@@ -8,5 +8,17 @@ git checkout k8s-autodiscovery
 cd ..
 wall -n "Getting everything into the right place"
 mv trace/* .
+cd k8s-yaml-files
+wall -n "Creating Kubernetes Secrets"
+kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
+kubectl create secret generic postgres-user --from-literal=token=postgres
+kubectl create secret generic postgres-password --from-literal=token=password
+wall -n "Starting services"
+kubectl apply -f redis-deploy.yaml
+kubectl apply -f postgres-deploy.yaml
+kubectl apply -f node-api.yaml
+kubectl apply -f pumps-service.yaml
+kubectl apply -f sensors-api.yaml
+kubectl apply -f frontend-service.yaml
 echo "complete">>status.txt
 
