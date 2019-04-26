39735b67aa46da645d36d6a080cbf24ba26f29e9
technovangelist <mattw@datadoghq.com>
2019-04-26 16:41:19 -0400

    asht

--- k8s4/background.sh
+++ k8s4/background.sh
@@ -24,16 +24,14 @@ cd k8s-yaml-files
 # sudo sed -i '9i\ \ strategy:\n    type: RollingUpdate\n    rollingUpdate:\n      maxUnavailable: 1\n      maxSurge: 1\n' frontend-service.yaml
 
 
-# sudo sed -i '81,83d' datadog-agent.yaml
-# sudo sed -i '78,79d' datadog-agent.yaml
-# sudo sed -i '42,56d' datadog-agent.yaml
-# sudo sed -i '44,45d' frontend-service.yaml
-# sudo sed -i '40,41d' frontend-service.yaml
-# sudo sed -i '36,39d' node-api.yaml
-# sudo sed -i '42,43d' pumps-service.yaml
-# sudo sed -i '36,37d' pumps-service.yaml
-# sudo sed -i '38,39d' sensors-api.yaml
-# sudo sed -i '34,35d' sensors-api.yaml
+sudo sed -i '49,56d' datadog-agent.yaml
+sudo sed -i '44,45d' frontend-service.yaml
+sudo sed -i '40,41d' frontend-service.yaml
+sudo sed -i '36,39d' node-api.yaml
+sudo sed -i '42,43d' pumps-service.yaml
+sudo sed -i '36,37d' pumps-service.yaml
+sudo sed -i '38,39d' sensors-api.yaml
+sudo sed -i '34,35d' sensors-api.yaml
 
 # rm datadog-agent.yaml
 wall -n "Creating Kubernetes Secrets"
