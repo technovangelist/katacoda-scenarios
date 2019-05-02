d8a2af31b4c6b8a479df314e3cc6d077782146eb
technovangelist <mattw@datadoghq.com>
2019-05-02 06:35:20 -0400

    move stuff around

--- k8s2-tags/background.sh
+++ k8s2-tags/background.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # mkdir k8s-yaml-files
 apt install datamash
-launch.sh
+#launch.sh
 touch status.txt
 
 wall -n "Starting services"
-kubectl apply -f k8s-yaml-files/lotsofpods.yaml
-kubectl apply -f k8s-yaml-files/datadog.yaml
+# kubectl apply -f k8s-yaml-files/lotsofpods.yaml
+# kubectl apply -f k8s-yaml-files/datadog.yaml
 
 echo "complete">>/root/status.txt
 
