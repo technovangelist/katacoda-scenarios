ccf7fd8370e5b2a8efb5b409880ae6f9e0a569a2
technovangelist <mattw@datadoghq.com>
2019-05-02 06:36:17 -0400

    cleaning up tags

--- k8s2-tags/background.sh
+++ k8s2-tags/background.sh
@@ -4,9 +4,9 @@ apt install datamash
 #launch.sh
 touch status.txt
 
-wall -n "Starting services"
+# wall -n "Starting services"
 # kubectl apply -f k8s-yaml-files/lotsofpods.yaml
 # kubectl apply -f k8s-yaml-files/datadog.yaml
 
-echo "complete">>/root/status.txt
+# echo "complete">>/root/status.txt
 
