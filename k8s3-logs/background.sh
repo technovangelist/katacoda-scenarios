4387691ee1b2286cd7dce8639889708ae55f0d7f
technovangelist <mattw@datadoghq.com>
2019-05-06 15:14:46 -0400

    asht

--- k8s3-logs/background.sh
+++ k8s3-logs/background.sh
@@ -11,18 +11,18 @@ cd ..
 wall -n "Getting everything into the right place"
 mv trace/* .
 cd k8s-yaml-files
-sudo sed -i '76,78d' datadog-agent.yaml #pointerdir in volumes
-sudo sed -i '73,74d' datadog-agent.yaml #pointerdir in vol mounts
-sudo sed -i '59,60d' datadog-agent.yaml #the logs and apm env vars
-sudo sed -i '42,53d' datadog-agent.yaml #the logs and apm env vars
-# sudo sed -i '16d' datadog-agent.yaml #hostnetwork
-sudo sed -i '41,42d' frontend-service.yaml #dd_logs_injection
-sudo sed -i '37,38d' frontend-service.yaml # datadog service name
-sudo sed -i '36,39d' node-api.yaml # service name and logs injection
-sudo sed -i '42,43d' pumps-service.yaml # datadogservice name
-sudo sed -i '36,37d' pumps-service.yaml #logs injections
-sudo sed -i '38,39d' sensors-api.yaml #logs injection
-sudo sed -i '34,35d' sensors-api.yaml #service name
+# sudo sed -i '76,78d' datadog-agent.yaml #pointerdir in volumes
+# sudo sed -i '73,74d' datadog-agent.yaml #pointerdir in vol mounts
+# sudo sed -i '59,60d' datadog-agent.yaml #the logs and apm env vars
+# sudo sed -i '42,53d' datadog-agent.yaml #the logs and apm env vars
+# # sudo sed -i '16d' datadog-agent.yaml #hostnetwork
+# sudo sed -i '41,42d' frontend-service.yaml #dd_logs_injection
+# sudo sed -i '37,38d' frontend-service.yaml # datadog service name
+# sudo sed -i '36,39d' node-api.yaml # service name and logs injection
+# sudo sed -i '42,43d' pumps-service.yaml # datadogservice name
+# sudo sed -i '36,37d' pumps-service.yaml #logs injections
+# sudo sed -i '38,39d' sensors-api.yaml #logs injection
+# sudo sed -i '34,35d' sensors-api.yaml #service name
 
 # rm datadog-agent.yaml
 wall -n "Creating Kubernetes Secrets"
