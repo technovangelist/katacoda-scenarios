91ab596fc0ac0854376cba25394227589361ab9c
technovangelist <mattw@datadoghq.com>
2019-05-14 16:07:06 -0400

    update scripts

--- k8s1-install/foreground.sh
+++ k8s1-install/foreground.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-launch.sh
+
 while [ ! -f "/usr/local/bin/prepenvironment" ]; do
   sleep 0.3
 done
 sleep 0.3
-cat > ddapikey.yml <<EOL
----
-datadog_api_key: ${DD_API_KEY}
-EOL
+
 kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
 clear
 
 
 prepenvironment
 
-{ echo "Provisioning Complete"; } 2> /dev/null
