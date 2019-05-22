0cd2cf29cd19fa33ae38e17624533278745faef4
technovangelist <mattw@datadoghq.com>
2019-05-21 20:14:13 -0400

    asht

--- logsintro1/foreground.sh
+++ logsintro1/foreground.sh
@@ -3,5 +3,6 @@
 # docker pull nginx:1.16.0
 
 git clone https://github.com/DataDog/TrainingEnvironment.git
-cp -r TrainingEnvironment/logsintro .
+cp -r TrainingEnvironment/logsintro/* .
 rm -rf TrainingEnvironment
+sed -i 's|datadog/agent:latest|datadog/agent:6.11.1|' docker-compose.yml
