309eafc0374d053ab99cf539a39f0b51082008f0
technovangelist <mattw@datadoghq.com>
2019-06-04 13:58:23 -0400

    asht

--- logsintro1/background.sh
+++ logsintro1/background.sh
@@ -1,5 +1,1 @@
 # echo "DD_API_KEY=thisisabogusapikey" >> apikey.env
-cat > apikey.env <<EOL
----
-DD_API_KEY=${DD_API_KEY}
-EOL
--- logsintro1/foreground.sh
+++ logsintro1/foreground.sh
@@ -1,6 +1,10 @@
 # docker pull datadog/agent:6.11.1
 # docker pull redis:5.0.5
 # docker pull nginx:1.16.0
+cat > apikey.env <<EOL
+---
+DD_API_KEY=${DD_API_KEY}
+EOL
 
 git clone https://github.com/DataDog/TrainingEnvironment.git
 cp -r TrainingEnvironment/logsintro/* .
