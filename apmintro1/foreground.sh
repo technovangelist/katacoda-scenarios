2a49c306877d7feae70f064837283522f79862dd
technovangelist <mattw@datadoghq.com>
2019-06-11 12:51:06 -0400

    asht

--- apmintro1/foreground.sh
+++ apmintro1/foreground.sh
@@ -12,6 +12,6 @@ rm -rf TrainingEnvironment
 
 # sed -i 's|datadog/agent:6.2.1|datadog/agent:6.11.0|' docker-compose.yml
 
-perl -pi -e "s|datadog/agent:6.2.1|datadog/agent:6.11.0\n    labels\n      com.datadoghq.ad.check_names: '[\"disk\"]\n      com.datadoghq.ad.init_configs: '[{}]'\n      com.datadoghq.ad.instances: '[{\"mount_point_blacklist\": \"/host/proc/sys/fs/binfmt_misc\asht\"}]'|" step00/docker-compose.yml
+perl -pi -e "s|datadog/agent:6.2.1|datadog/agent:6.11.0\n\t\tlabels\n      com.datadoghq.ad.check_names: '[\"disk\"]\n      com.datadoghq.ad.init_configs: '[{}]'\n      com.datadoghq.ad.instances: '[{\"mount_point_blacklist\": \"/host/proc/sys/fs/binfmt_misc\asht\"}]'|" step00/docker-compose.yml
 ./start00
 
