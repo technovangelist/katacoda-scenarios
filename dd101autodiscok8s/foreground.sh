caa36254ad9e22d1bc12625bee29cafdefff682e
technovangelist <mattw@datadoghq.com>
2019-07-13 22:56:06 -0400

    asht
    Signed-off-by: technovangelist <mattw@datadoghq.com>

--- dd101autodiscok8s/foreground.sh
+++ dd101autodiscok8s/foreground.sh
@@ -3,7 +3,7 @@ if [ ! -f "/root/provisioned" ]; then
 kubectl create -f "https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/rbac/clusterrole.yaml"
 kubectl create -f "https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/rbac/serviceaccount.yaml"
 kubectl create -f "https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/rbac/clusterrolebinding.yaml"
-kubectl create secret generic datadog-secret --from-literal api-key=$DD_API_KEY
+kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
 fi
 
 clear
