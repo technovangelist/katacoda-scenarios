c9fb1f0f8a34aae5bfba918c019e97e54481c489
Matt Williams <matt.williams@ddmatt2.local>
2019-09-11 16:54:16 -0400

    ashtasht

--- newdd101-3integration/background.sh
+++ newdd101-3integration/background.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 touch /root/startq
+mkdir -p /root/lab
 apt-get update -y
 apt install redis-server nginx -y
 sudo systemctl enable redis-server.service
@@ -7,4 +8,6 @@ sudo sed -i '2i maxmemory 256mb' /etc/redis/redis.conf
 sudo sed -i '3i maxmemory-policy allkeys-lru' /etc/redis/redis.conf
 sudo systemctl restart redis-server.service
 /etc/init.d/nginx start
+ln -s /etc/redis /root/lab/redis
+ln -s /etc/nginx /root/lab/nginx
 touch /root/end
