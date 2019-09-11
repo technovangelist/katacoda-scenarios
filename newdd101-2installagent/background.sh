62d72766e99cb4e11e4d0885d8956045fcc8c399
Matt Williams <matt.williams@ddmatt2.local>
2019-09-10 20:53:44 -0400

    docker compose shell

--- newdd101-2installagent/background.sh
+++ newdd101-2installagent/background.sh
@@ -1,2 +1,5 @@
+mkdir /etc/datadog-agent
+apt install direnv
+echo "echo 'This directory is actually located at /etc/datadog-agent'" > .envrc
 ln -s /etc/datadog-agent /root/vmfiles
 mkdir /root/dockerfiles
