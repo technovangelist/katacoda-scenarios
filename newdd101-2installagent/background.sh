ef8a83ce3b6b054304bea4321ab78fe3b5d8c0db
Matt Williams <matt.williams@ddmatt2.local>
2019-09-10 21:08:57 -0400

    asht

--- newdd101-2installagent/background.sh
+++ newdd101-2installagent/background.sh
@@ -1,4 +1,3 @@
-printf 'eval "$(direnv hook bash)"' >> /root/.bashrc
 mkdir /etc/datadog-agent
 apt install direnv
 # echo "echo 'This directory is actually located at /etc/datadog-agent'" > /etc/datadog-agent/.envrc
