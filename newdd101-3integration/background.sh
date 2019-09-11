837b7af436c1cd78979e79d730dc61c30f139438
Matt Williams <matt.williams@ddmatt2.local>
2019-09-11 17:06:21 -0400

    ashtasht

--- newdd101-3integration/background.sh
+++ newdd101-3integration/background.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 touch /root/startq
 mkdir -p /root/lab
+touch /etc/datadog-agent/empty
 bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
 ln -s /etc/datadog-agent /root/lab/datadog
 apt-get update -y
