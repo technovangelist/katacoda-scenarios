8b0fa4bb9f0579b3d6368c42e5fddcb1b5b485d4
technovangelist <matt.williams@datadoghq.com>
2020-03-04 15:03:58 -0500

    wait for all the yaml files
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8scourse2platform-1/foreground.sh
+++ k8scourse2platform-1/foreground.sh
@@ -1,4 +1,9 @@
+while [ ! `ls -l *.yaml | wc -l ` -eq 4 ]; do
+  sleep 0.3
+done
 k apply -f db.yaml
 k apply -f advertisements.yaml
 k apply -f discounts.yaml
 k apply -f frontend.yaml
+clear
+prepenvironment
