8270818f141fdd88f8801dddb657400c371309f8
technovangelist <matt.williams@datadoghq.com>
2020-03-04 16:00:46 -0500

    add background
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

---
+++ k8scourse2platform-1/background.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+touch status.txt
+echo "">/root/status.txt
+# if [ ! -f "/root/provisioned" ]; then
+#   apt install datamash
+# fi
+
+echo "complete">>/root/status.txt
--- k8scourse2platform-2/background.sh
+++ k8scourse2platform-2/background.sh
@@ -1,1 +1,8 @@
-echo "bacground"
+#!/bin/bash
+touch status.txt
+echo "">/root/status.txt
+# if [ ! -f "/root/provisioned" ]; then
+#   apt install datamash
+# fi
+
+echo "complete">>/root/status.txt
