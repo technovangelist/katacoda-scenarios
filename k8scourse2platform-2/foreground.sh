fb92eb04ab33a0e18f00275d2ffc056094480aec
technovangelist <matt.williams@datadoghq.com>
2020-03-04 15:45:34 -0500

    add helm section
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8scourse2platform-2/foreground.sh
+++ k8scourse2platform-2/foreground.sh
@@ -1,1 +1,11 @@
-echo "foreground"
+while [ ! `ls -l /root/k8s-yaml-files/*.yaml | wc -l ` -eq 5 ]; do
+  sleep 0.3
+done
+k create secret generic datadog-api --from-literal=token=$DD_API_KEY
+
+k apply -f k8s-yaml-files/db.yaml
+k apply -f k8s-yaml-files/advertisements.yaml
+k apply -f k8s-yaml-files/discounts.yaml
+k apply -f k8s-yaml-files/frontend.yaml
+clear
+prepenvironment
