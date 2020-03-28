fc012ce8cfc4437f93057b4a3ea0d75643513dfd
technovangelist <matt.williams@datadoghq.com>
2020-03-28 16:29:14 -0400

    ashtasht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8s2-tags/background.sh
+++ k8s2-tags/background.sh
@@ -3,16 +3,7 @@ touch status.txt
 echo "">/root/status.txt
 if [ ! -f "/root/provisioned" ]; then
   apt install datamash
-  git clone https://github.com/kubernetes-sigs/metrics-server.git
-  cd metrics-server
-  kubectl create -f deploy/kubernetes
-  cd ..
-  git clone https://github.com/kubernetes/kube-state-metrics.git
-  cd kube-state-metrics
-  kubectl apply -f examples/standard
-  cd ..
-  rm -rf kube-state-metrics
-  rm -rf metrics-server
+
 
 fi
 
--- k8scourse2platform-2/background.sh
+++ k8scourse2platform-2/background.sh
@@ -7,6 +7,17 @@ chmod 700 get_helm.sh
 ./get_helm.sh
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 
+  git clone https://github.com/kubernetes-sigs/metrics-server.git
+  cd metrics-server
+  kubectl create -f deploy/kubernetes
+  cd ..
+  git clone https://github.com/kubernetes/kube-state-metrics.git
+  cd kube-state-metrics
+  kubectl apply -f examples/standard
+  cd ..
+  rm -rf kube-state-metrics
+  rm -rf metrics-server
+
 # if [ ! -f "/root/provisioned" ]; then
 #   apt install datamash
 # fi
