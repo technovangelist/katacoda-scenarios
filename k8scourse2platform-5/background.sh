a147d7ef728a03e81e1d8d580a412aeecaaa456a
technovangelist <matt.williams@datadoghq.com>
2020-05-15 17:08:32 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8scourse2platform-5/background.sh
+++ k8scourse2platform-5/background.sh
@@ -3,12 +3,13 @@ touch status.txt
 echo "">/root/status.txt
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all 
-
+source ~/.bashrc
 
 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl && sudo apt-get install -y conntrack
+sudo apt autoremove -y
 
 minikube start --driver=none
 
@@ -21,7 +22,7 @@ echo "Waiting for kubernetes to start" >>/root/status.txt
 #   sleep 1
 # done
 echo "Waiting for all nodes to be ready" >>/root/status.txt
-while [ "$( kubectl get nodes --no-headers 2>/dev/null| awk '{print $2}'|xargs )" !=  "Ready Ready" ]; do
+while [ "$( kubectl get nodes --no-headers 2>/dev/null| awk '{print $2}'|xargs )" !=  "Ready" ]; do
   sleep 1
 done
 echo "Kubernetes ready.">>/root/status.txt
