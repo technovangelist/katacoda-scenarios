c66034852f79eeecf23c398464bf323370941d52
technovangelist <matt.williams@datadoghq.com>
2020-05-15 17:16:38 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8scourse2platform-5/background.sh
+++ k8scourse2platform-5/background.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 touch status.txt
 echo "">/root/status.txt
+wall -n installing fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all 
 source ~/.bashrc
 
-
+wall -n installing minikube
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin
 
+wall -n installing kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl && sudo apt-get install -y conntrack
 sudo apt autoremove -y
 
+wall -n starting minikube
 minikube start --driver=none
 
+wall -n installing helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
@@ -21,8 +25,10 @@ echo "Waiting for kubernetes to start" >>/root/status.txt
 # while [ "$( kubectl get nodes --no-headers 2>/dev/null | wc -l )" != "2" ]; do
 #   sleep 1
 # done
+
 echo "Waiting for all nodes to be ready" >>/root/status.txt
 while [ "$( kubectl get nodes --no-headers 2>/dev/null| awk '{print $2}'|xargs )" !=  "Ready" ]; do
+  kubectl get nodes --no-headers 2>/dev/null| awk '{print $2}'|xargs
   sleep 1
 done
 echo "Kubernetes ready.">>/root/status.txt
