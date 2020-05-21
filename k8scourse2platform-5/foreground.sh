8d1f20c19da4bc9111b023439e85b7bfc78c57ce
technovangelist <matt.williams@datadoghq.com>
2020-05-21 09:06:44 -0400

    ash
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8scourse2platform-5/foreground.sh
+++ k8scourse2platform-5/foreground.sh
@@ -2,11 +2,8 @@ while [ ! `ls -l /root/k8s-yaml-files/*.yaml 2>/dev/null | wc -l ` -eq 5 ]; do
   sleep 0.3
 done
 clear
-ssh -q -o StrictHostKeyChecking=no root@node01 mkdir -p /root/k8s-yaml-files/kube_apiserver_metrics.d  > /dev/null
+
 clear
-while [ ! -f "/usr/local/bin/syncyaml.sh" ]; do
-  sleep 0.2
-done
 
 
 clear
