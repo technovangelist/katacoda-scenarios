6777e3e0ec5ac68418176ab2c2f78fd7c14cd736
technovangelist <matt.williams@datadoghq.com>
2020-05-20 21:54:56 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8scourse2platform-5/foreground.sh
+++ k8scourse2platform-5/foreground.sh
@@ -1,9 +1,7 @@
 while [ ! `ls -l /root/k8s-yaml-files/*.yaml 2>/dev/null | wc -l ` -eq 5 ]; do
   sleep 0.3
 done
-while [ ! `k get nodes 2>/dev/null | wc -l ` -eq 2 ]; do
-  sleep 0.3
-done
+
 
 
 ssh -q -o StrictHostKeyChecking=no root@node01 mkdir -p /root/k8s-yaml-files/kube_apiserver_metrics  > /dev/null
