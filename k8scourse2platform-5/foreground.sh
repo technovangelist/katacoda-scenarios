b223b5ed1684c7b883af6c050d373cdd6318182f
technovangelist <matt.williams@datadoghq.com>
2020-05-20 21:44:11 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8scourse2platform-5/foreground.sh
+++ k8scourse2platform-5/foreground.sh
@@ -6,7 +6,7 @@ while [ ! 'k get nodes 2>/dev/null | wc -l ' eq 2]; do
 done
 
 sed -i 's|--DATADOG_API_KEY--|datadog/agent:6.11.1|' docker-compose.yml
-ssh root@node01 mkdir -p /root/k8s-yaml-files/kube_apiserver_metrics
+ssh -o StrictHostKeyChecking=no root@node01 mkdir -p /root/k8s-yaml-files/kube_apiserver_metrics 
 syncyaml.sh &
 
 clear
