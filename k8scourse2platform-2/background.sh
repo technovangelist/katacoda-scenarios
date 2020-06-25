879ffb27ca83ba4d122289104eacb1a8d1c22dd6
technovangelist <matt.williams@datadoghq.com>
2020-06-25 16:51:04 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8scourse2platform-2/background.sh
+++ k8scourse2platform-2/background.sh
@@ -15,22 +15,22 @@ while [ "$( kubectl get nodes --no-headers 2>/dev/null| awk '{print $2}'|xargs )
   sleep 1
 done
 echo "Kubernetes ready.">>/root/status.txt
-  # git clone https://github.com/kubernetes-sigs/metrics-server.git
-  # cd metrics-server
-  # kubectl create -f deploy/kubernetes
-  # cd ..
-  # git clone https://github.com/kubernetes/kube-state-metrics.git
-  # cd kube-state-metrics
-  # kubectl apply -f examples/standard
-  # cd ..
-  # rm -rf kube-state-metrics
-  # rm -rf metrics-server
+
 kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
 
 kubectl apply -f k8s-yaml-files/db.yaml
 kubectl apply -f k8s-yaml-files/advertisements.yaml
 kubectl apply -f k8s-yaml-files/discounts.yaml
 kubectl apply -f k8s-yaml-files/frontend.yaml
+
+(
+  set -x; cd "$(mktemp -d)" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}" &&
+  tar zxvf krew.tar.gz &&
+  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
+  "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
+  "$KREW" update
+)
 # if [ ! -f "/root/provisioned" ]; then
 #   apt install datamash
 # fi
--- k8scourse2platform-2/foreground.sh
+++ k8scourse2platform-2/foreground.sh
@@ -4,7 +4,7 @@ done
 while [ ! 'k get nodes 2>/dev/null | wc -l ' eq 2]; do
   sleep 0.3
 done
-
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 sed -i 's|--DATADOG_API_KEY--|datadog/agent:6.11.1|' docker-compose.yml
 
 clear
