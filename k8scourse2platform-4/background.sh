8b0038ced3578db0ff3c7b9387f68f2d8db1bdc0
technovangelist <matt.williams@datadoghq.com>
2020-06-26 19:18:05 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8scourse2platform-4/background.sh
+++ k8scourse2platform-4/background.sh
@@ -41,14 +41,7 @@ kubectl apply -f k8s-yaml-files/frontend.yaml
 # if [ ! -f "/root/provisioned" ]; then
 #   apt install datamash
 # fi
-(
-  set -x; cd "$(mktemp -d)" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}" &&
-  tar zxvf krew.tar.gz &&
-  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
-  "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
-  "$KREW" update
-)
+
 
 
 echo "complete">>/root/status.txt
--- k8scourse2platform-4/foreground.sh
+++ k8scourse2platform-4/foreground.sh
@@ -1,4 +1,12 @@
-while [ ! `ls -l /root/k8s-yaml-files/*.yaml 2>/dev/null | wc -l ` -eq 6 ]; do
+(
+  set -x; cd "$(mktemp -d)" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}" &&
+  tar zxvf krew.tar.gz &&
+  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
+  "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
+  "$KREW" update
+)
+while [ ! `ls -l /root/k8s-yaml-files/*.yaml 2>/dev/null | wc -l ` -eq 5 ]; do
   sleep 0.3
 done
 while [ ! 'k get nodes 2>/dev/null | wc -l ' eq 2]; do
