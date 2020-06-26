05a432d4998aa9520d392f9fc6a56b1eba50f9a8
technovangelist <matt.williams@datadoghq.com>
2020-06-26 17:06:55 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8scourse2platform-4/foreground.sh
+++ k8scourse2platform-4/foreground.sh
@@ -5,8 +5,6 @@ while [ ! 'k get nodes 2>/dev/null | wc -l ' eq 2]; do
   sleep 0.3
 done
 
-sed -i 's|--DATADOG_API_KEY--|datadog/agent:6.11.1|' docker-compose.yml
-
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 kubectl completion bash >/etc/bash_completion.d/kubectl
 complete -F __start_kubectl k
