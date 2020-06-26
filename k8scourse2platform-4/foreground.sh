0b5658271db1d10c77dce1d3566de13e610f2feb
technovangelist <matt.williams@datadoghq.com>
2020-06-26 11:24:44 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8scourse2platform-4/foreground.sh
+++ k8scourse2platform-4/foreground.sh
@@ -1,4 +1,4 @@
-while [ ! `ls -l /root/k8s-yaml-files/*.yaml 2>/dev/null | wc -l ` -eq 6 ]; do
+while [ ! `ls -l /root/k8s-yaml-files/*.yaml 2>/dev/null | wc -l ` -eq 5 ]; do
   sleep 0.3
 done
 while [ ! 'k get nodes 2>/dev/null | wc -l ' eq 2]; do
