7520c8d7cb5231ffe215ec5324fcfc83a3f3d255
technovangelist <matt.williams@datadoghq.com>
2020-06-28 17:40:55 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8scourse2platform-4/background.sh
+++ k8scourse2platform-4/background.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 touch status.txt
 echo "">/root/status.txt
-
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-helm repo add stable https://kubernetes-charts.storage.googleapis.com
+sudo snap install helm --classic
+# curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+# chmod 700 get_helm.sh
+# ./get_helm.sh
+# helm repo add stable https://kubernetes-charts.storage.googleapis.com
 echo "Waiting for kubernetes to start" >>/root/status.txt
 while [ "$( kubectl get nodes --no-headers 2>/dev/null | wc -l )" != "2" ]; do
   sleep 1
@@ -14,6 +14,17 @@ echo "Waiting for all nodes to be ready" >>/root/status.txt
 while [ "$( kubectl get nodes --no-headers 2>/dev/null| awk '{print $2}'|xargs )" !=  "Ready Ready" ]; do
   sleep 1
 done
+(
+  set -x; cd "$(mktemp -d)" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}" &&
+  tar zxvf krew.tar.gz &&
+  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
+  "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
+  "$KREW" update
+)
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+kubectl krew install match-name
+
 echo "Kubernetes ready.">>/root/status.txt
   # git clone https://github.com/kubernetes-sigs/metrics-server.git
   # cd metrics-server
--- k8scourse2platform-4/foreground.sh
+++ k8scourse2platform-4/foreground.sh
@@ -1,11 +1,4 @@
-(
-  set -x; cd "$(mktemp -d)" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}" &&
-  tar zxvf krew.tar.gz &&
-  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
-  "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
-  "$KREW" update
-)
+
 while [ ! `ls -l /root/k8s-yaml-files/*.yaml 2>/dev/null | wc -l ` -eq 6 ]; do
   sleep 0.3
 done
@@ -17,7 +10,7 @@ export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 kubectl completion bash >/etc/bash_completion.d/kubectl
 complete -F __start_kubectl k
 kubectl create secret generic datadog-secret --from-literal=api-key=$DD_API_KEY
-kubectl krew install match-name
+# kubectl krew install match-name
 cd k8s-yaml-files
 clear
 prepenvironment
