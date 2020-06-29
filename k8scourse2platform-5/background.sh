483b78a5f004dd707bb921fc73380295a7577827
technovangelist <matt.williams@datadoghq.com>
2020-06-29 06:41:29 -0400

    clean up foreground
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8scourse2platform-5/background.sh
+++ k8scourse2platform-5/background.sh
@@ -2,9 +2,6 @@
 touch status.txt
 echo "">/root/status.txt
 
-curl -L https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-
 echo "Waiting for kubernetes to start" >>/root/status.txt
 while [ "$( kubectl get nodes --no-headers 2>/dev/null | wc -l )" != "2" ]; do
   sleep 1
@@ -13,6 +10,11 @@ echo "Waiting for all nodes to be ready" >>/root/status.txt
 while [ "$( kubectl get nodes --no-headers 2>/dev/null| awk '{print $2}'|xargs )" !=  "Ready Ready" ]; do
   sleep 1
 done
+echo "kubernetes nodes up and running">>/root/status.txt
+
+curl -L https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+echo "helm installed" >>/root/status.txt
 
 (
   set -x; cd "$(mktemp -d)" &&
@@ -24,17 +26,16 @@ done
 )
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 kubectl krew install match-name
+echo "krew installed" >>/root/status.txt
 
-echo "Kubernetes ready.">>/root/status.txt
+
 
 kubectl create secret generic datadog-secret --from-literal=api-key=$DD_API_KEY 
 
-kubectl apply -f k8s-yaml-files/db.yaml
-kubectl apply -f k8s-yaml-files/advertisements.yaml
-kubectl apply -f k8s-yaml-files/discounts.yaml
-kubectl apply -f k8s-yaml-files/frontend.yaml
-# if [ ! -f "/root/provisioned" ]; then
-#   apt install datamash
-# fi
+kubectl apply -f /root/k8s-yaml-files/db.yaml
+kubectl apply -f /root/k8s-yaml-files/advertisements.yaml
+kubectl apply -f /root/k8s-yaml-files/discounts.yaml
+kubectl apply -f /root/k8s-yaml-files/frontend.yaml
+echo "Kubernetes ready.">>/root/status.txt
 
-echo "complete">>/root/status.txt
+echo "background complete">>/root/status.txt
--- k8scourse2platform-5/foreground.sh
+++ k8scourse2platform-5/foreground.sh
@@ -2,15 +2,24 @@ while [ ! `ls -l /root/k8s-yaml-files/*.yaml 2>/dev/null | wc -l ` -eq 5 ]; do
   sleep 0.3
 done
 clear
+while ! grep -q "helm installed" /root/status.txt; do
+  sleep 0.3
+  echo "helm not installed"
+done
+helm install datadogagent --set datadog.apiKey=$DD_API_KEY --set datadog.appKey=$DD_APP_KEY -f values.yaml stable/datadog
 
+while ! grep -q "krew installed" /root/status.txt; do 
+  echo "krew not installed"
+  sleep 0.3
+done
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
 kubectl completion bash >/etc/bash_completion.d/kubectl
 complete -F __start_kubectl k
+
 kubectl create secret generic datadog-secret --from-literal=api-key=$DD_API_KEY
 
 cd k8s-yaml-files
-
-helm install datadogagent --set datadog.apiKey=$DD_API_KEY --set datadog.appKey=$DD_APP_KEY -f values.yaml stable/datadog
 
 clear
 prepenvironment
