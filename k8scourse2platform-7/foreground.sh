2878c36046e2bd4a3c7e243c306ce739e920b5be
technovangelist <matt.williams@datadoghq.com>
2020-06-30 17:11:42 -0400

    clean logs from coredns
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8scourse2platform-7/foreground.sh
+++ k8scourse2platform-7/foreground.sh
@@ -2,17 +2,10 @@ while [ ! `ls -l /root/k8s-yaml-files/*.yaml 2>/dev/null | wc -l ` -eq 6 ]; do
   sleep 0.3
 done
 clear
-# while ! grep -q "helm installed" /root/status.txt; do
-#   sleep 1
-#   echo "Waiting for Helm"
-# done
+
 statuscheck helm
 helm install datadogagent --set datadog.apiKey=$DD_API_KEY --set datadog.appKey=$DD_APP_KEY -f /root/k8s-yaml-files/values.yaml stable/datadog
 
-# while ! grep -q "krew installed" /root/status.txt; do 
-#   echo "Waiting for kubectl plugins"
-#   sleep 1
-# done
 statuscheck krew
 
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
