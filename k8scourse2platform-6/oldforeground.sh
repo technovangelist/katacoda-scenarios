0e07924c18b91eea80d9b8a85d8d4075d30cfbe6
technovangelist <matt.williams@datadoghq.com>
2020-06-29 21:56:29 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

---
+++ k8scourse2platform-6/oldforeground.sh
@@ -0,0 +1,13 @@
+# while [ ! `ls -l /root/k8s-yaml-files/*.yaml 2>/dev/null | wc -l ` -eq 5 ]; do
+#   sleep 0.3
+# done
+# while [ ! 'k get nodes 2>/dev/null | wc -l ' eq 2]; do
+#   sleep 0.3
+# done
+source ~/.bashrc
+sed -i 's|--DATADOG_API_KEY--|datadog/agent:6.11.1|' docker-compose.yml
+
+alias k=kubectl
+
+# clear
+prepenvironment
