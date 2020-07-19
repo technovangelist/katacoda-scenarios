6a0d1043fffcc2254fb93f6159c3418c88e46e64
technovangelist <matt.williams@datadoghq.com>
2020-07-19 10:15:56 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- dash2020-101/getting-started/background.sh
+++ dash2020-101/getting-started/background.sh
@@ -3,3 +3,4 @@ curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 statusupdate toolsv2
 echo '{"files.exclude": {".*": true}}' > ~/.vscode/settings.json
 mkdir ~/editor
+statusupdate complete
--- dash2020-101/getting-started/foreground.sh
+++ dash2020-101/getting-started/foreground.sh
@@ -1,5 +1,5 @@
 statuscheck toolsv1
 statuscheck toolsv2
-prepenvironment
 echo -n "Image Built: \c"
 cat /opt/.builddate
+prepenvironment
