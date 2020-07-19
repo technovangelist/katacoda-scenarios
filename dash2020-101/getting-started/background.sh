68c2d912410a0df81256a0d5caa3bda6b331abdc
technovangelist <matt.williams@datadoghq.com>
2020-07-19 15:32:40 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- dash2020-101/getting-started/background.sh
+++ dash2020-101/getting-started/background.sh
@@ -1,6 +1,5 @@
-statusupdate toolsv1
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
-statusupdate toolsv2
+statusupdate tools
 echo '{"files.exclude": {".*": true}}' > ~/.vscode/settings.json
 mkdir ~/editor
 statusupdate complete
--- dash2020-101/getting-started/foreground.sh
+++ dash2020-101/getting-started/foreground.sh
@@ -1,6 +1,4 @@
-statuscheck toolsv1
-echo "run this only after toolsv1 is installed"
-statuscheck toolsv2
-echo "run this only after toolsv1 is installed"
 debuginfo east
 prepenvironment
+ln -s /ecommworkshop /root/workshop
+cd workshop
