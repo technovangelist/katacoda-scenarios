ac1ac769a8a408d5d96d0de8592f40b1f9b9599a
technovangelist <matt.williams@datadoghq.com>
2020-07-25 20:43:41 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- dash2020-101/4-using-the-app/background.sh
+++ dash2020-101/4-using-the-app/background.sh
@@ -1,7 +1,7 @@
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 statusupdate tools
 mkdir -p /root/lab
-sln /ecommworkshop /root/lab/ecommerceapp
+ln -s /ecommworkshop /root/lab/ecommerceapp
 statusupdate createdirs
 docker pull redis
 docker pull datadog/agent
