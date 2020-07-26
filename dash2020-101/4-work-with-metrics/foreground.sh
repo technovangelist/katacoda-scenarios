e59e6454830067d0f6b323c183f371145d5e8028
technovangelist <matt.williams@datadoghq.com>
2020-07-26 09:36:26 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- dash2020-101/4-work-with-metrics/foreground.sh
+++ dash2020-101/4-work-with-metrics/foreground.sh
@@ -3,6 +3,6 @@ statuscheck tools
 ln -s /ecommworkshop /root/lab/ecommerceapp
 clear 
 debuginfo east
-prepenvironment
-# cd /root/lab
 cd /root/lab/ecommerceapp/docker-compose-files
+prepenvironment
+
