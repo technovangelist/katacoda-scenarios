0de4a2524beaaf677bd1afcfb274539e14f703f2
technovangelist <matt.williams@datadoghq.com>
2020-07-26 11:29:20 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- dash2020-101/4-work-with-metrics/background.sh
+++ dash2020-101/4-work-with-metrics/background.sh
@@ -8,7 +8,10 @@ git pull
 statusupdate createdirs
 docker-compose -f /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml pull
 statusupdate dockerpulls
-
-statusupdate complete
 POSTGRES_USER=postgres POSTGRES_PASSWORD=postgres docker-compose -f /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml up -d
+while [ -z `docker-compose -f /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml ps -q db` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose -f /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml ps -q db)` ]; do
+  sleep .5
+done
+docker-compose -f ./docker-compose-fixed-instrumented.yml exec db psql -U postgres -c  "create user datadog with password 'password';grant pg_monitor to datadog;"
+statusupdate complete
 
--- dash2020-101/4-work-with-metrics/foreground.sh
+++ dash2020-101/4-work-with-metrics/foreground.sh
@@ -3,12 +3,6 @@ statuscheck tools
 ln -s /ecommworkshop /root/lab/ecommerceapp
 cd /root/lab/ecommerceapp/docker-compose-files
 clear 
-while [ -z `docker-compose -f /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml ps -q db` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose -f /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml ps -q db)` ]; do
-  sleep .5
-  echo "trying again"
-done
 
-
-docker-compose -f ./docker-compose-fixed-instrumented.yml exec db psql -U postgres -c  "create user datadog with password 'password';grant pg_monitor to datadog;"
 prepenvironment
 
