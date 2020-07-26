6ebd3bb77f655459a45ad4b36e6dad6fe4faf64c
technovangelist <matt.williams@datadoghq.com>
2020-07-26 13:12:33 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- dash2020-101/4-work-with-metrics/foreground.sh
+++ dash2020-101/4-work-with-metrics/foreground.sh
@@ -4,7 +4,7 @@ ln -s /ecommworkshop /root/lab/ecommerceapp
 cd /root/lab/ecommerceapp/docker-compose-files
 clear 
 statuscheck dbup
-docker-compose -f ./docker-compose-fixed-instrumented.yml exec db psql -U postgres -c  "create user datadog with password 'password';grant pg_monitor to datadog;"
+docker-compose -f ./docker-compose-fixed-instrumented.yml exec -T -d db psql -U postgres -c  "create user datadog with password 'password';grant pg_monitor to datadog;"
 
 
 clear
