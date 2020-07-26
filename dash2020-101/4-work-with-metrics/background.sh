f907c29e6e8999e8d29769fabe01e2f1a16b111a
technovangelist <matt.williams@datadoghq.com>
2020-07-26 16:28:08 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- dash2020-101/4-work-with-metrics/background.sh
+++ dash2020-101/4-work-with-metrics/background.sh
@@ -8,7 +8,7 @@ git pull
 statusupdate createdirs
 docker-compose -f /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml pull
 statusupdate dockerpulls
-POSTGRES_USER=postgres POSTGRES_PASSWORD=postgres docker-compose -f /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml up -d
+statuscheck dockerstart
 while [ -z `docker-compose -f /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml ps -q db` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose -f /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml ps -q db)` ]; do
   sleep .5
 done
--- dash2020-101/4-work-with-metrics/foreground.sh
+++ dash2020-101/4-work-with-metrics/foreground.sh
@@ -2,6 +2,10 @@ clear
 statuscheck tools
 ln -s /ecommworkshop /root/lab/ecommerceapp
 cd /root/lab/ecommerceapp/docker-compose-files
+clear
+statuscheck dockerpulls
+POSTGRES_USER=postgres POSTGRES_PASSWORD=postgres docker-compose -f /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml up -d
+statusupdate dockerstart
 clear 
 statuscheck dbup
 docker-compose -f ./docker-compose-fixed-instrumented.yml exec -T -d db psql -U postgres -c  "create user datadog with password 'password';grant pg_monitor to datadog;"
