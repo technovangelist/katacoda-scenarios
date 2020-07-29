cd58ef0812ed165d20eae2b6669fc0a075b3b4e8
technovangelist <matt.williams@datadoghq.com>
2020-07-29 05:18:22 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- dash2020-101/9-real-user-monitoring/foreground.sh
+++ dash2020-101/9-real-user-monitoring/foreground.sh
@@ -1,16 +1,12 @@
 clear
 statuscheck tools
-ln -s /ecommworkshop /root/lab/ecommerceapp
-cd /root/lab/ecommerceapp/
+statuscheck pulls
+mv /ecommworkshop /root
+mv /root/docker-compose.yaml /root/ecommworkshop/docker-compose-files/
+cd /root/ecommworkshop/docker-compose-files
 clear
-statuscheck dockerpulls
-# POSTGRES_USER=postgres POSTGRES_PASSWORD=postgres docker-compose -f /root/lab/ecommerceapp/docker-compose.yaml up -d
-# statusupdate dockerstart
-clear 
-# statuscheck dbup
-# docker-compose -f ./docker-compose.yaml exec -T -d db psql -U postgres -c  "create user datadog with password 'password';grant pg_monitor to datadog;grant SELECT ON pg_stat_database to datadog;"
 
-
+cd ecommworkshop
 clear
 prepenvironment
 
