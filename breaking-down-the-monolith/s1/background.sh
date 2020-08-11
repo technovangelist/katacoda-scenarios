121630644a24d1c0cc28d99703b0aab992825571
technovangelist <matt.williams@datadoghq.com>
2020-08-11 17:14:01 -0400

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- breaking-down-the-monolith/s1/background.sh
+++ breaking-down-the-monolith/s1/background.sh
@@ -2,16 +2,16 @@
 
 # Make a new home and clone the app
 # mkdir /ecommerce-observability
-git clone https://github.com/DataDog/ecommerce-workshop /root/ecommerceapp && cd /ecommerceapp
+git clone https://github.com/DataDog/ecommerce-workshop /root/ecommerce-observability && cd /ecommerceapp
 
 # Stop instrumenting the ads/discounts services
-sudo sed -i 's/ddtrace-run //g' /root/ecommerceapp/docker-compose-files/docker-compose-fixed-instrumented.yml
+sudo sed -i 's/ddtrace-run //g' /root/ecommerce-observability/docker-compose-files/docker-compose-fixed-instrumented.yml
 
 # Some special env vars
 {
   echo "export POSTGRES_USER=postgres"
   echo "export POSTGRES_PASSWORD=postgres"
-  echo "export COMPOSE_FILE=/root/ecommerceapp/docker-compose-files/docker-compose-fixed-instrumented.yml"
+  echo "export COMPOSE_FILE=/root/ecommerce-observability/docker-compose-files/docker-compose-fixed-instrumented.yml"
 } >> ~/.bashrc
 
 # Utility commands to manage the cluster
@@ -19,7 +19,7 @@ sudo sed -i 's/ddtrace-run //g' /root/ecommerceapp/docker-compose-files/docker-c
   echo "alias application_start='docker-compose up'"
   echo "alias application_stop='docker-compose stop && docker-compose rm -f'"
   echo "alias application_reload='application_stop && application_start'"
-  echo "alias generate_traffic='/root/ecommerceapp/gor --input-file-loop --input-file requests_0.gor --output-http \"http://localhost:3000\"'"
+  echo "alias generate_traffic='/root/ecommerce-observability/gor --input-file-loop --input-file requests_0.gor --output-http \"http://localhost:3000\"'"
   echo "alias break_service='docker-compose kill discounts'"
   echo "alias fix_service='docker-compose up discounts'"
 } >> ~/.bashrc
--- breaking-down-the-monolith/s1/foreground.sh
+++ breaking-down-the-monolith/s1/foreground.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /root/ecommerceapp
+cd /root/ecommerce-observability
 ls
 cd
 clear
