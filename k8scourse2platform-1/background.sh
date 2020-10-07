4c595c4249b0378fac6caa8b1d843a0be48220da
technovangelist <matt.williams@datadoghq.com>
2020-10-07 11:18:09 -0400

    get latest tools
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- k8scourse2platform-1/background.sh
+++ k8scourse2platform-1/background.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 touch status.txt
 echo "">/root/status.txt
+
 # if [ ! -f "/root/provisioned" ]; then
 #   apt install datamash
 # fi
