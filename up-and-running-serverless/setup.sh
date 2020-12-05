8a47187fa2650bdcb4b5aa693a7528749b1c4ea3
technovangelist <matt.williams@datadoghq.com>
2020-12-04 17:06:08 -0800

    asht
    Signed-off-by: technovangelist <matt.williams@datadoghq.com>

--- up-and-running-serverless/setup.sh
+++ up-and-running-serverless/setup.sh
@@ -3,7 +3,7 @@ ENDPOINT="https://gvgx8c2xfj.execute-api.us-east-1.amazonaws.com/get-credentials
 RESULT=$( curl -s "$ENDPOINT" )
 mkdir ~/.aws
 touch ~/.aws/credentials
-echo "[default]" >> ~/.aws/credentials
-echo "aws_access_key_id=$( echo $RESULT | jq -r .key )" >> ~/.aws/credentials
-echo "aws_secret_access_key=$( echo $RESULT | jq -r .secret )" >> ~/.aws/credentials
+echo "[default]" > ~/.aws/credentials
+echo "aws_access_key_id=$( echo $RESULT | jq -r .key )" > ~/.aws/credentials
+echo "aws_secret_access_key=$( echo $RESULT | jq -r .secret )" > ~/.aws/credentials
 cd /root/workshop
