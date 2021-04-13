f40fafd3bf62a4ca19953d83a8f6798091f49ac2
Matt Williams <m@technovangelist.com>
2021-04-13 11:29:48 -0700

    asht
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- apmintrov2/instrumentapp2/foreground.sh
+++ apmintrov2/instrumentapp2/foreground.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export POSTGRES_USER=postgres
-export POSTGRES_PASSWORD=postgres
-cd /ecommworkshop/docker-compose-files
-docker-compose -f docker-compose-broken-no-apm-instrumentation.yml up -d
-clear
+# export POSTGRES_USER=postgres
+# export POSTGRES_PASSWORD=postgres
+# cd /ecommworkshop/docker-compose-files
+# docker-compose -f docker-compose-broken-no-apm-instrumentation.yml up -d
+# clear
 
-envready
-statusupdate complete
-clear
+# envready
+# statusupdate complete
+# clear
 
-prepenvironment
+# prepenvironment
