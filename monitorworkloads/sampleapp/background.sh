7cc43822d368e94ccbec60203526f6e6826ac3b5
Matt Williams <m@technovangelist.com>
2021-05-10 12:49:51 -0700

    asht
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- monitorworkloads/sampleapp/background.sh
+++ monitorworkloads/sampleapp/background.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
+
 statusupdate complete
--- monitorworkloads/sampleapp/foreground.sh
+++ monitorworkloads/sampleapp/foreground.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
+docker-compose -f /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml up -d
 echo 'here'
 prepenvironment
