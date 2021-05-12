e08af1f7a87bf17dc412184c852fd0b14ccb53bc
Matt Williams <m@technovangelist.com>
2021-05-12 14:34:24 -0700

    asht
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- monitorworkloads/sampleapp/foreground.sh
+++ monitorworkloads/sampleapp/foreground.sh
@@ -4,7 +4,7 @@ export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 docker-compose -f /ecommworkshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml up -d
 cd /ecommworkshop || exit
-./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" &
+./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" > /dev/null 2>&1 &
 echo 'here'
 clear
 statuscheck labtools
