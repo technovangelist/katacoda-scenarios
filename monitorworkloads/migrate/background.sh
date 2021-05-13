152bcb463f7a6457e6d07498710b7f21cc60dc5e
Matt Williams <m@technovangelist.com>
2021-05-13 12:45:25 -0700

    s2
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- monitorworkloads/migrate/background.sh
+++ monitorworkloads/migrate/background.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
+statusupdate labtools
 
-
+mkdir -p /root/workshop
 statusupdate complete
--- monitorworkloads/migrate/foreground.sh
+++ monitorworkloads/migrate/foreground.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export POSTGRES_USER=postgres
-export POSTGRES_PASSWORD=postgres
-docker-compose -f /ecommworkshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml up -d
-cd /ecommworkshop || exit
-./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" &
-echo 'here'
+statuscheck labtools
+clear
 prepenvironment
