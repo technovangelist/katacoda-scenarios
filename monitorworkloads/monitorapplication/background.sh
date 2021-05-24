5c50d059bb27a676086d77bb7e43a617d87c024c
Matt Williams <m@technovangelist.com>
2021-05-23 21:19:00 -0700

    asht
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- monitorworkloads/monitorapplication/background.sh
+++ monitorworkloads/monitorapplication/background.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
+statusupdate labtools
 
+mkdir -p /root/workshop
 
+mkdir /root/completedfiles
+launch.sh
+helm repo add datadog https://helm.datadoghq.com
+helm repo add stable https://charts.helm.sh/stable
+helm repo update
+cp -r /ecommworkshop/* /root/workshop
+cp /root/values.yaml /root/workshop/deploy/datadog/helm-values.yaml
+statusupdate k8s
+# launch.sh
 statusupdate complete
