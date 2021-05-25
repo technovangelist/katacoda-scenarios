5bb02dd230497abb24337ac28aac8b44aa5a66d9
Matt Williams <m@technovangelist.com>
2021-05-25 09:22:45 -0700

    asht
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- monitorworkloads/monitorapplication/background.sh
+++ monitorworkloads/monitorapplication/background.sh
@@ -6,6 +6,9 @@ mkdir -p /root/workshop
 
 mkdir /root/completedfiles
 launch.sh
+sed -i '/- --port=0/d' /etc/kubernetes/manifests/kube-scheduler.yaml
+sed -i '/- --port=0/d' /etc/kubernetes/manifests/kube-controller-manager.yaml
+kubectl delete pod "$(kubectl get pods --no-headers -o custom-columns=":metadata.name" -l app=katacoda-cloud-provider -n kube-system)" -n kube-system
 helm repo add datadog https://helm.datadoghq.com
 helm repo add stable https://charts.helm.sh/stable
 helm repo update
