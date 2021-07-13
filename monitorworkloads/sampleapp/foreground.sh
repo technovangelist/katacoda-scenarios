63ead28b8cd85933202134136a527c1a1f553df8
Matt Williams <m@technovangelist.com>
2021-07-13 12:01:45 -0700

    asht
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- monitorworkloads/sampleapp/foreground.sh
+++ monitorworkloads/sampleapp/foreground.sh
@@ -4,7 +4,13 @@ export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 docker-compose -f /ecommworkshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml up -d
 docker-compose -f /ecommworkshop/deploy/docker-compose/docker-compose-traffic-replay.yml up -d
-cp /ecommworkshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml ~
+mkdir -p /root/deploy/docker-compose/
+cp /ecommworkshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml /root/deploy/docker-compose/
+cp -r /ecommworkshop/ads-service-fixed /root/ads-service-fixed
+cp -r /ecommworkshop/discounts-service-fixed /root/discounts-service-fixed
+cp -r /ecommworkshop/store-frontend-instrumented-fixed /root/store-frontend-instrumented-fixed
+cp -r /ecommworkshop/storedog /root/storedog
+
 # cd /ecommworkshop || exit
 # ./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" > /dev/null 2>&1 &
 echo 'here'
