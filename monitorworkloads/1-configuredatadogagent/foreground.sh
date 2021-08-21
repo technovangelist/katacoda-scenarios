0c0b17ae3b8cb7821262c672ab4b83b10f10d67f
Matt Williams <m@technovangelist.com>
2021-08-20 21:44:27 -0700

    asht
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- monitorworkloads/1-configuredatadogagent/foreground.sh
+++ monitorworkloads/1-configuredatadogagent/foreground.sh
@@ -7,9 +7,10 @@ clear
 statuscheck k8s
 helm install datadogagent-controlplane datadog/datadog --set datadog.apiKey=$DD_API_KEY -f /root/workshop/deploy/datadog/helm-values.yaml
 # sleep 5
+wait-for-it $(k get service/db -o jsonpath='{.spec.clusterIP}'):6443
 waitfork8s
 k wait deploy/datadogagent-controlplane-cluster-agent --for condition=available
-
+k config get-clusters | wc -l
 helm install datadogagent datadog/datadog --set datadog.apiKey=$DD_API_KEY -f /root/workshop/deploy/datadog/helm-node-values.yaml
 kubectl wait --for=condition=ready pod -l app=datadogagent
 k apply -f /root/workshop/deploy/generic-k8s/ecommerce-app/db.yaml
