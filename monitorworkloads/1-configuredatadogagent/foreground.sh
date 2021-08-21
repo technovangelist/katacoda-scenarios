64e618bab35130b16b897565f07232aa20110ab2
Matt Williams <m@technovangelist.com>
2021-08-20 22:15:49 -0700

    asht
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- monitorworkloads/1-configuredatadogagent/foreground.sh
+++ monitorworkloads/1-configuredatadogagent/foreground.sh
@@ -13,17 +13,30 @@ while ! k get pods
 do
     sleep 1
 done
+k get deploy/db -o jsonpath='{.status}' |jq
+sleep 2
+k get deploy/db -o jsonpath='{.status}' |jq
+sleep 2
+k get deploy/db -o jsonpath='{.status}' |jq
+sleep 2
+k get deploy/db -o jsonpath='{.status}' |jq
+sleep 2
+k get deploy/db -o jsonpath='{.status}' |jq
+sleep 2
+k get deploy/db -o jsonpath='{.status}' |jq
+
 # k wait deploy/datadogagent-controlplane-cluster-agent --for condition=available
 # k config get-clusters 
 
 # k get pods
 # k get nodes
 helm install datadogagent datadog/datadog --set datadog.apiKey=$DD_API_KEY -f /root/workshop/deploy/datadog/helm-node-values.yaml
-kubectl wait --for=condition=ready pod -l app=datadogagent
+# kubectl wait --for=condition=ready pod -l app=datadogagent
 k apply -f /root/workshop/deploy/generic-k8s/ecommerce-app/db.yaml
+k get pod/db
 # clear
 # wait-for-it $(k get service/db -o jsonpath='{.spec.clusterIP}'):5432
-k wait deploy/db --for condition=available
+# k wait deploy/db --for condition=available
 k apply -f /root/workshop/deploy/generic-k8s/ecommerce-app/discounts.yaml
 k apply -f /root/workshop/deploy/generic-k8s/ecommerce-app/advertisements.yaml
 k apply -f /root/workshop/deploy/generic-k8s/ecommerce-app/frontend.yaml
