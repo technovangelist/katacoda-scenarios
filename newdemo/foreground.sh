c4d63e391fc024561ab1298149ff45f8475ddee8
Matt Williams <m@technovangelist.com>
2021-09-20 16:01:41 -0700

    asht
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- newdemo/foreground.sh
+++ newdemo/foreground.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # Put stuff here that runs in a foreground process, visible to the learner.
 
-statuscheck "workspace"
-cd /root/lab
+# statuscheck "workspace"
+# cd /root/lab
 
-# Store learner environment variables for background.sh to access
-# ** Set DD_ENV to a unique value for your scenario. E.g. dd-201 **
-printf "DD_API_KEY=$DD_API_KEY\n\
-DD_APP_KEY=$DD_APP_KEY\n\
-POSTGRES_USER=postgres\n\
-DD_ENV=my-environment\n\ 
-POSTGRES_PASSWORD=postgres" > .env 
+# # Store learner environment variables for background.sh to access
+# # ** Set DD_ENV to a unique value for your scenario. E.g. dd-201 **
+# printf "DD_API_KEY=$DD_API_KEY\n\
+# DD_APP_KEY=$DD_APP_KEY\n\
+# POSTGRES_USER=postgres\n\
+# DD_ENV=my-environment\n\ 
+# POSTGRES_PASSWORD=postgres" > .env 
 
-clear
-statusupdate "environment-variables"
-statuscheck "storedog"
+# clear
+# statusupdate "environment-variables"
+# statuscheck "storedog"
 
-# Display trial account credentials in terminal
-statusupdate complete
-prepenvironment
+# # Display trial account credentials in terminal
+# statusupdate complete
+# prepenvironment
