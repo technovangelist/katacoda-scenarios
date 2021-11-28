b6985b8e8e1cda2215b0c6012a674d51e914d18d
Matt Williams <m@technovangelist.com>
2021-11-27 16:26:07 -0800

    asht
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- monitorworkloads/monitorapplication/foreground.sh
+++ monitorworkloads/monitorapplication/foreground.sh
@@ -3,7 +3,7 @@ until [ -f "/usr/local/bin/prepenvironment" ]; do
   sleep 0.3
 done
 statuscheck labtools
-clear
+# clear
 statuscheck k8s
 # helm install datadogagent-controlplane datadog/datadog --set datadog.apiKey=$DD_API_KEY --set datadog.appKey=$DD_APP_KEY -f /root/workshop/deploy/datadog/helm-values.yaml
 # helm install datadogagent datadog/datadog --set datadog.apiKey=$DD_API_KEY --set datadog.appKey=$DD_APP_KEY -f /root/workshop/deploy/datadog/helm-node-values.yaml
@@ -14,5 +14,5 @@ sed -i "s/replacetheapplicationid/$DD_APPLICATION_ID/g" /root/workshop/deploy/ge
 helm install datadogagent datadog/datadog --set datadog.apiKey=$DD_API_KEY --set datadog.appKey=$DD_APP_KEY -f /root/workshop/deploy/datadog/helm-values.yaml
 k apply -f /root/workshop/deploy/generic-k8s/ecommerce-app/
 cd /root/workshop
-prepenvironment
+# prepenvironment
 
